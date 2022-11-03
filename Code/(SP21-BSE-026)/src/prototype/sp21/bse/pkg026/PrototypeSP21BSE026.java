23f0f24538bd87d92d481755759e6cc1834ac769
waleed-khan11 <115110572+waleed-khan11@users.noreply.github.com>
2022-11-03 20:57:53 +0500

    Add files via upload

---
+++ Code/(SP21-BSE-026)/src/prototype/sp21/bse/pkg026/PrototypeSP21BSE026.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype.sp21.bse.pkg026;
+
+/**
+ *
+ * @author Hp
+ */
+public class PrototypeSP21BSE026
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        //CheckList l = new CheckList();
+        //l.setVisible(true);
+        DonateBlood d = new DonateBlood();
+        d.setVisible(true);
+    }
+    
+}

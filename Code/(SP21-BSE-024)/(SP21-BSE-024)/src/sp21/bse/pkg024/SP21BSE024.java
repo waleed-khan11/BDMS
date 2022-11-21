dbb0b288c475c95a3592e00fafdce7e5de464235
Tayyab-Rafique <115549503+Tayyab-Rafique@users.noreply.github.com>
2022-11-21 16:27:59 +0300

    Add files via upload

---
+++ Code/(SP21-BSE-024)/(SP21-BSE-024)/src/sp21/bse/pkg024/SP21BSE024.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp21.bse.pkg024;
+
+/**
+ *
+ * @author Hp
+ */
+public class SP21BSE024
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
